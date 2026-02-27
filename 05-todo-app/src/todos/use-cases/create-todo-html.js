4cba6c82d00fc61ebdeca1ed257b9d3184f38107
Axl <actcelchirinos1@gmail.com>
2026-02-27 00:41:15 -0400

    feat: implement new todo input handling, enhance todo rendering with completed state, and improve error handling for element selection

--- 05-todo-app/src/todos/use-cases/create-todo-html.js
+++ 05-todo-app/src/todos/use-cases/create-todo-html.js
@@ -8,10 +8,22 @@ export const createTodoHtml = ( todo ) => {
 
     if( !todo ) throw new Error('A TODD object is required');
 
-    const html = `<h1>&{ todo.description }</h1>`;
+    const { done, description, id } = todo;
+
+    const html = `
+        <div class="view">
+            <input class="toggle" type="checkbox" ${ done ? 'ckecked' : '' } >
+            <label>${ description }</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    `;
 
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    liElement.setAttribute('data-id', id);
+
+    if( done ) liElement.classList.add('completed');
 
     return liElement;
     
--- 05-todo-app/src/todos/use-cases/render-todos.js
+++ 05-todo-app/src/todos/use-cases/render-todos.js
@@ -1,6 +1,8 @@
 import { Todo } from "../models/todo.model";
 import { createTodoHtml } from "./";
 
+let element;
+
 /**
  * 
  * @param {String} elementId 
@@ -8,8 +10,11 @@ import { createTodoHtml } from "./";
  */
 export const renderTodos = ( elementId, todos = [] ) => {
 
-    // TODO: referencia
-    const element = document.querySelector( elementId );
+    if ( !element ) element = document.querySelector( elementId );
+
+    if ( !element ) throw new Error(`Element ${ elementId } not found`);
+
+    element.innerHTML = '';
 
     todos.forEach(todo => {
         element.append( createTodoHtml(todo) );
