3c9967d1051d0bd4c7310b7137985eee6a01ac8f
Axl <actcelchirinos1@gmail.com>
2026-02-26 23:15:21 -0400

    feat: add UUID dependency, enhance todo functionality with new methods for managing todos, and implement rendering logic for displaying todos

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -11,6 +11,8 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra del realidad'),
     ],
     filter: Filters.All,
 }
@@ -20,6 +22,84 @@ const initStore = () => {
     console.log('InitStore 🦾');
 }
 
-export default { 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param { Filters } filter
+ * @returns 
+ */
+const getTodos = ( filter = Filters.All ) => {
+    switch( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done === true );
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+        default:
+            throw new Error('Option  ${ filter } is not valid.');
+    }
+}
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    if( !description ) throw new Error('Description is required');
+
+    state.todos.push( new Todo(description) );
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+
+    state.todos = state.todos.map( todo => {
+        if( todo.id === todoId ) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
+}
+
+const deleteCompleted = ( ) => {
+    state.todos = state.todos.filter( todo => todo.done );
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = new Filters;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+export default {
     initStore,
+    loadStore,
+    getTodos,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
 }
---
+++ 05-todo-app/src/todos/use-cases/render-todos.js
@@ -0,0 +1,17 @@
+import { Todo } from "../models/todo.model";
+import { createTodoHtml } from "./";
+
+/**
+ * 
+ * @param {String} elementId 
+ * @param {Todo} todos 
+ */
+export const renderTodos = ( elementId, todos = [] ) => {
+
+    // TODO: referencia
+    const element = document.querySelector( elementId );
+
+    todos.forEach(todo => {
+        element.append( createTodoHtml(todo) );
+    });
+}
